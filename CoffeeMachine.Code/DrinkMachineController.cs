f938b5a1a891ff87ab5351fea1ec47a3d66984e3
Vinh Vu <vinh.vu65@gmail.com>
2022-05-12 13:45:04 +1000

    - Test 1 - Green

--- CoffeeMachine.Code/DrinkMachineController.cs
+++ CoffeeMachine.Code/DrinkMachineController.cs
@@ -2,5 +2,10 @@ namespace CoffeeMachine.Code;
 
 public class DrinkMachineController
 {
+    public string DrinkMakerProtocol { get; set; }
     
+    public void CreateDrinkMakerProtocol(IDrink drinkRequested)
+    {
+        DrinkMakerProtocol = drinkRequested.DrinkCode;
+    }
 }
---
+++ CoffeeMachine.Code/IDrink.cs
@@ -0,0 +1,6 @@
+namespace CoffeeMachine.Code;
+
+public interface IDrink
+{
+    public string DrinkCode { get; set; }
+}
