fe2795280b1d14aedcf3c6a415e955926f74bfa5
Vinh Vu <vinh.vu65@gmail.com>
2022-05-13 09:42:17 +1000

    - Test 6 - Red, Drink catalog should return relevant drink code when ordered drink matches catalog item

---
+++ CoffeeMachine.Tests/DrinksCatalogTests.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using CoffeeMachine.Code;
+using NSubstitute;
+using Xunit;
+
+namespace CoffeeMachine.Tests;
+
+public class DrinksCatalogTests
+{
+    private readonly IDrink _drink = Substitute.For<IDrink>();
+    private readonly DrinksCatalog _sut;
+
+    public DrinksCatalogTests()
+    {
+        _sut = new DrinksCatalog();
+    }
+    
+    [Theory]
+    [InlineData(Products.Coffee, "C")]
+    [InlineData(Products.HotChocolate, "H")]
+    [InlineData(Products.Tea, "T")]
+
+    public void MatchDrinkCode_ShouldReturnDrinkCode_WhenDrinkTypesMatchInOrderAndCatalog(Products drinkType, string expectedCode)
+    {
+        _drink.DrinkType = drinkType;
+        
+        var result = _sut.MatchDrinkCode(_drink);
+        
+        Assert.Equal(expectedCode, result);
+    }
+}
