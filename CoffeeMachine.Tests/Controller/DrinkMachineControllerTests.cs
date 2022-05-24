1ec39d7afc6f21ad73437bf829fdc03bef8eadab
Vinh Vu <vinh.vu65@gmail.com>
2022-05-24 10:33:24 +1000

    - Refactor - Add DrinkMaker interface to send commands

--- CoffeeMachine.Tests/Controller/DrinkMachineControllerTests.cs
+++ CoffeeMachine.Tests/Controller/DrinkMachineControllerTests.cs
@@ -12,10 +12,12 @@ public class DrinkMachineControllerTests
     private readonly DrinkMachineController _sut;
     private readonly IDrinksCatalog _catalog = Substitute.For<IDrinksCatalog>();
     private readonly IProtocolBuilder _protocolBuilder = Substitute.For<IProtocolBuilder>();
+    private readonly IDrinkMaker _drinkMaker = Substitute.For<IDrinkMaker>();
 
     public DrinkMachineControllerTests()
     {
-        _sut = new DrinkMachineController(_catalog, _protocolBuilder);
+        _sut = new DrinkMachineController(_catalog, _protocolBuilder, _drinkMaker);
+        _drinkOrder = new DrinkOrder(DrinkType.Coffee, 2, false);
     }
 
     [Fact]
@@ -23,9 +25,8 @@ public class DrinkMachineControllerTests
     {
         var sampleRecord = new CatalogRecord(DrinkType.Coffee, "A", 0m);
         _catalog.QueryCatalog(Arg.Any<DrinkType>()).Returns(sampleRecord);
-        _drinkOrder = new DrinkOrder(DrinkType.Coffee, 2, false);
         
-        _sut.CreateDrinkMakerCommand(_drinkOrder, 10m);
+        _sut.ManageDrinkOrder(_drinkOrder, 10m);
 
         _catalog.Received(1).QueryCatalog(_drinkOrder.DrinkType);
     }
@@ -38,9 +39,8 @@ public class DrinkMachineControllerTests
     {
         var sampleRecord = new CatalogRecord(DrinkType.Coffee, "A", 0m);
         _catalog.QueryCatalog(Arg.Any<DrinkType>()).Returns(sampleRecord);
-        _drinkOrder = new DrinkOrder(DrinkType.Coffee, 2, false);
 
-        _sut.CreateDrinkMakerCommand(_drinkOrder, moneyInserted);
+        _sut.ManageDrinkOrder(_drinkOrder, moneyInserted);
         
         _protocolBuilder.Received(1).BuildDrinkCommand("A", Arg.Any<DrinkOrder>());
         _protocolBuilder.Received(0).BuildMessageCommand(Arg.Any<string>());
@@ -54,9 +54,8 @@ public class DrinkMachineControllerTests
     {
         var sampleRecord = new CatalogRecord(DrinkType.Coffee, "A", 10m);
         _catalog.QueryCatalog(Arg.Any<DrinkType>()).Returns(sampleRecord);
-        _drinkOrder = new DrinkOrder(DrinkType.Coffee, 2, false);
 
-        _sut.CreateDrinkMakerCommand(_drinkOrder, moneyInserted);
+        _sut.ManageDrinkOrder(_drinkOrder, moneyInserted);
         
         _protocolBuilder.Received(1).BuildMessageCommand(Arg.Any<string>());
         _protocolBuilder.Received(0).BuildDrinkCommand("A", Arg.Any<DrinkOrder>());
@@ -72,11 +71,10 @@ public class DrinkMachineControllerTests
         _catalog.QueryCatalog(Arg.Any<DrinkType>()).Returns(sampleRecord);
         var expectedMessage = $"{moneyDifference}";
         _protocolBuilder.BuildMessageCommand(Arg.Any<string>()).Returns(expectedMessage);
-        _drinkOrder = new DrinkOrder(DrinkType.Coffee, 2, false);
 
-        var result = _sut.CreateDrinkMakerCommand(_drinkOrder, moneyInserted);
+        _sut.ManageDrinkOrder(_drinkOrder, moneyInserted);
         
-        Assert.Contains(expectedMessage, result);
+        _drinkMaker.Received(1).SendCommand(expectedMessage);
     }
 
     // Integration tests:
@@ -89,11 +87,11 @@ public class DrinkMachineControllerTests
         var drink = new DrinkOrder(drinkType, sugar, false);
         var catalog = new DrinksCatalog();
         var builder = new ProtocolBuilder();
-        var sut = new DrinkMachineController(catalog, builder);
+        var sut = new DrinkMachineController(catalog, builder, _drinkMaker);
 
-        var result = sut.CreateDrinkMakerCommand(drink, 10m);
+        sut.ManageDrinkOrder(drink, 10m);
         
-        Assert.Equal(expected, result);
+        _drinkMaker.Received(1).SendCommand(expected);
     }
     
     [Theory]
@@ -105,11 +103,11 @@ public class DrinkMachineControllerTests
         var drink = new DrinkOrder(drinkType, sugar, true);
         var catalog = new DrinksCatalog();
         var builder = new ProtocolBuilder();
-        var sut = new DrinkMachineController(catalog, builder);
+        var sut = new DrinkMachineController(catalog, builder, _drinkMaker);
 
-        var result = sut.CreateDrinkMakerCommand(drink, 10m);
+        sut.ManageDrinkOrder(drink, 10m);
         
-        Assert.Equal(expected, result);
+        _drinkMaker.Received(1).SendCommand(expected);
     }
 
     [Fact]
@@ -117,11 +115,12 @@ public class DrinkMachineControllerTests
     {
         var drink = new DrinkOrder(DrinkType.OrangeJuice, 2, true);
         var catalog = new DrinksCatalog();
-        var builder = new ProtocolBuilder();
-        var sut = new DrinkMachineController(catalog, builder);
+        var sut = new DrinkMachineController(catalog, _protocolBuilder, _drinkMaker);
+        var message = "Can't make a hot orange juice";
+        _protocolBuilder.BuildMessageCommand(Arg.Any<string>()).Returns(message);
 
-        var result = sut.CreateDrinkMakerCommand(drink, 10m);
+        sut.ManageDrinkOrder(drink, 10m);
 
-        Assert.StartsWith("M:", result);
+        _drinkMaker.Received(1).SendCommand(message);
     }
 }
