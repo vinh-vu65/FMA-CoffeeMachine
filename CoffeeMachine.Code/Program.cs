b7ec0dc1bcc8cdac9cbc4e5e60b018a182494a4c
Vinh Vu <vinh.vu65@gmail.com>
2022-05-24 14:19:20 +1000

    Add N substitute to project

--- CoffeeMachine.Code/Program.cs
+++ CoffeeMachine.Code/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CoffeeMachine.Code.Controller;
+using CoffeeMachine.Code.Models;
+using CoffeeMachine.Code.Services;
+using NSubstitute;
 
-Console.WriteLine("Hello, World!");
+IDrinkMaker drinkMaker = Substitute.For<IDrinkMaker>();
+IReportGenerator reportGenerator = new ReportGenerator();
+var hotChoc = new DrinkOrder(DrinkType.HotChocolate, 2, false);
+var coffee = new DrinkOrder(DrinkType.Coffee, 2, true);
+var tea = new DrinkOrder(DrinkType.Tea, 2, false);
+var OJ = new DrinkOrder(DrinkType.OrangeJuice,0 , false);
+
+var catalog = new DrinksCatalog();
+var builder = new ProtocolBuilder();
+var sut = new DrinkMachineController(catalog, builder, drinkMaker, reportGenerator);
+
+sut.ManageDrinkOrder(hotChoc, 10m);
+sut.ManageDrinkOrder(coffee, 10m);
+sut.ManageDrinkOrder(tea, 10m);
+sut.ManageDrinkOrder(OJ, 10m);
+
+sut.PrintDrinkHistory();
