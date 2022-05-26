569c11d8dc6c6fbe33c4461ba18afa0b6b648677
Vinh Vu <vinh.vu65@gmail.com>
2022-05-26 13:58:02 +1000

    - Test - Green

--- CoffeeMachine.Code/Program.cs
+++ CoffeeMachine.Code/Program.cs
@@ -4,6 +4,7 @@ using CoffeeMachine.Code.Services;
 using NSubstitute;
 
 var drinkMaker = Substitute.For<IDrinkMaker>();
+var quantityChecker = Substitute.For<IBeverageQuantityChecker>();
 var reportGenerator = new ReportGenerator();
 var hotChoc = new DrinkOrder(DrinkType.HotChocolate, 2, false);
 var coffee = new DrinkOrder(DrinkType.Coffee, 2, true);
@@ -12,7 +13,7 @@ var OJ = new DrinkOrder(DrinkType.OrangeJuice,0 , false);
 
 var catalog = new DrinksCatalog();
 var builder = new ProtocolBuilder();
-var sut = new DrinkMachineManager(catalog, builder, drinkMaker, reportGenerator);
+var sut = new DrinkMachineManager(catalog, builder, drinkMaker, reportGenerator, quantityChecker);
 
 sut.ManageDrinkOrder(hotChoc, 10m);
 sut.ManageDrinkOrder(coffee, 10m);
