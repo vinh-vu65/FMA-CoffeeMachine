7627690f30243b3f0a0627d3f0e62834635ffb03
Vinh Vu <vinh.vu65@gmail.com>
2022-05-31 15:33:12 +1000

    Add predetermined dates for tests

--- CoffeeMachine.Tests/Services/ReportGeneratorTests.cs
+++ CoffeeMachine.Tests/Services/ReportGeneratorTests.cs
@@ -8,11 +8,12 @@ namespace CoffeeMachine.Tests.Services;
 
 public class ReportGeneratorTests
 {
-    private ReportGenerator _sut;
-    private List<FulfilledDrinkOrder> _sampleHistory;
+    private readonly ReportGenerator _sut;
+    private readonly List<FulfilledDrinkOrder> _sampleHistory;
+    private readonly DateTime _filterDate;
     private readonly DrinkOrder _coffee = new(DrinkType.Coffee, 2, false);
     private readonly DrinkOrder _tea = new(DrinkType.Tea, 2, false);
-    private readonly DrinkOrder _hotChocolate = new(DrinkType.HotChocolate, 2, false);
+    private readonly DrinkOrder _hotChocolate = new(DrinkType.HotChocolate, 2, true);
 
     
     public ReportGeneratorTests()
@@ -20,30 +21,31 @@ public class ReportGeneratorTests
         _sut = new ReportGenerator();
         _sampleHistory = new List<FulfilledDrinkOrder>
         {
-            new(_coffee, DateTime.Now, 3m),
-            new(_coffee, DateTime.Now, 3m),
-            new(_coffee, DateTime.Now, 3m)
+            new(_coffee, new DateTime(2022, 5, 31, 10, 30, 0), 3m),
+            new(_tea, new DateTime(2022, 5, 31, 11, 30, 0), 3m),
+            new(_hotChocolate, new DateTime(2022, 5, 31, 11, 35, 0), 3m)
         };
+        _filterDate = new DateTime(2022, 5, 31);
     }
     
     [Fact]
     public void GenerateHistoryReport_ShouldIncludeAllDrinksSoldInThatDay_WhenGivenDrinkHistoryAndDate()
     {
-        var result = _sut.GenerateHistoryReport(_sampleHistory, DateTime.Today);
+        var result = _sut.GenerateHistoryReport(_sampleHistory, _filterDate);
 
-        Assert.Contains(_sampleHistory[0].DrinkOrder.ToString(), result);
-        Assert.Contains(_sampleHistory[1].DrinkOrder.ToString(), result);
-        Assert.Contains(_sampleHistory[2].DrinkOrder.ToString(), result);
+        Assert.Contains("Coffee, 2 sugar, Regular", result);
+        Assert.Contains("Tea, 2 sugar, Regular", result);
+        Assert.Contains("HotChocolate, 2 sugar, Extra hot", result);
     }
     
     [Fact]
     public void GenerateHistoryReport_ShouldIncludeAllDrinkTimeStampsSoldInThatDay_WhenGivenDrinkHistoryAndDate()
     {
-        var result = _sut.GenerateHistoryReport(_sampleHistory, DateTime.Today);
+        var result = _sut.GenerateHistoryReport(_sampleHistory, _filterDate);
 
-        Assert.Contains(_sampleHistory[0].TimePurchased.ToString("dddd, dd MMMM yyyy hh:mm tt"), result);
-        Assert.Contains(_sampleHistory[1].TimePurchased.ToString("dddd, dd MMMM yyyy hh:mm tt"), result);
-        Assert.Contains(_sampleHistory[2].TimePurchased.ToString("dddd, dd MMMM yyyy hh:mm tt"), result);
+        Assert.Contains("Tuesday, 31 May 2022 10:30 AM", result);
+        Assert.Contains("Tuesday, 31 May 2022 11:30 AM", result);
+        Assert.Contains("Tuesday, 31 May 2022 11:35 AM", result);
     }
 
     [Fact]
@@ -51,8 +53,8 @@ public class ReportGeneratorTests
     {
         var result = _sut.GenerateHistoryReport(_sampleHistory, DateTime.MinValue);
         
-        Assert.DoesNotContain(_sampleHistory[0].DrinkOrder.ToString(), result);
-        Assert.DoesNotContain(_sampleHistory[1].DrinkOrder.ToString(), result);
-        Assert.DoesNotContain(_sampleHistory[2].DrinkOrder.ToString(), result);
+        Assert.DoesNotContain("Tuesday, 31 May 2022 10:30 AM", result);
+        Assert.DoesNotContain("Tuesday, 31 May 2022 11:30 AM", result);
+        Assert.DoesNotContain("Tuesday, 31 May 2022 11:35 AM", result);
     }
 }
